c21f4682837a912bab88cc3fa8cf2952eb56cc4b
Russel Corbo <russelcorbo@gmail.com>
2023-07-19 16:01:50 -0400

    Magic Eightball game
    Made with Codeacademy

---
+++ magic_eightball.js
@@ -0,0 +1,38 @@
+let userName = "Michael";
+
+userName ? console.log(`Hello, ${userName}`) : console.log("Hello!");
+
+const userQuestion = "will I become a walrus tomorrow?"
+
+console.log(`${userName} you asked the question ${userQuestion}`);
+
+randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = "";
+
+switch (randomNumber) {
+  case 0:
+    console.log('It is certain');
+    break;
+  case 1:
+    console.log('It is decidedly so');
+    break;
+  case 2:
+    console.log('Reply hazy try again');
+    break;
+  case 3:
+    console.log('Cannot predict now');
+    break;
+  case 4:
+    console.log('Do not count on it');
+    break;
+  case 5:
+    console.log('My sources say no');
+    break;
+  case 6:
+    console.log('Outlook not so good');
+    break;
+  case 7:
+    console.log('Signs point to yes');
+    break;
+}
